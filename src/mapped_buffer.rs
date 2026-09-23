2700f7d5ca3473916c79e2b8eec27568db868654
admin <lzty.jw@gmail.com>
2026-09-23 16:02:57 +0800

    make MapBuffer more rusty

--- src/mapped_buffer.rs
+++ src/mapped_buffer.rs
@@ -1,19 +1,34 @@
+use core::slice;
 use std::ffi::c_void;
 
 use crate::{kernel_call::CallBy, traits::ExecuteKernelCall};
 
+/// Wrap and mange a memory range that mapped from kernel space(typically by `MemoryMap::mmap`)
 pub struct MapBuffer<'a, E>
 where
     E: ExecuteKernelCall,
 {
     pub(crate) mdl: *mut c_void,
     pub(crate) virt_u: *mut c_void,
+    pub(crate) length: usize,
     pub(crate) exploit: &'a E,
 }
 
 impl<E: ExecuteKernelCall> MapBuffer<'_, E> {
-    pub fn get(&self) -> *mut c_void {
+    pub fn as_ptr(&self) -> *const c_void {
         self.virt_u
+    }
+
+    pub fn as_ptr_mut(&self) -> *mut c_void {
+        self.virt_u
+    }
+
+    pub fn as_slice<T: Sized>(&self) -> &[T] {
+        unsafe { slice::from_raw_parts(self.virt_u.cast::<T>(), self.length) }
+    }
+
+    pub fn as_slice_mut<T: Sized>(&mut self) -> &mut [T] {
+        unsafe { slice::from_raw_parts_mut(self.virt_u.cast::<T>(), self.length) }
     }
 }
 
